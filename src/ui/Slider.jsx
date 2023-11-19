24463b2135ee569223a5be0be5546d3e6ef348a1
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2023-11-19 02:40:59 +0200

    up

--- src/ui/Slider.jsx
+++ src/ui/Slider.jsx
@@ -1,41 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import Overlay from "./Overlay";
-// import DotNav from "./DotNav";
 
 function Slider({ images }) {
   const [imgIndex, setImgIndex] = useState(0);
 
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     setImgIndex(index => {
-  //       if (index === images.length - 1) return 0;
-  //       return index + 1;
-  //     });
-  //   }, 3000);
-  // }, []);
-  // const timeoutRef = useRef(null);
-  // useEffect(() => {
-  //   (timeoutRef.current = setTimeout(() =>
-  //     setImgIndex(index => (index === images.length - 1 ? 0 : index + 1))
-  //   )),
-  //     2500;
+  useEffect(() => {
+    setInterval(() => {
+      setImgIndex(index => {
+        if (index === images.length - 1) return 0;
+        return index + 1;
+      });
+    }, 3000);
+    return () => {};
+  }, [images.length]);
 
-  //   return () => {};
-  // }, [images.length]);
-
-  // function showPrevImg() {
-  //   setImgIndex(index => {
-  //     if (index == 0) return images.length - 1;
-  //     return index - 1;
-  //   });
-  // }
-  // function showNextImg() {
-  //   setImgIndex(index => {
-  //     if (index === images.length - 1) return 0;
-  //     return index + 1;
-  //   });
-  // }
   return (
     <div className="bg-mainBlack text-mainWhite text-4xl relative rounded-t-xl lg:rounded-xl overflow-hidden ">
       <Overlay className="bg-mainBlack/20" />
