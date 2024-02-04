88f331fa49c4487f6f523cda809e0a5c74b5a00d
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2024-02-04 12:04:38 +0200

    resusable Input Component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,7 @@
-// import { Link } from "react-router-dom";
-
 import image from "../assets/images/traveling.jpg";
 import google from "../assets/images/google.svg";
 import { Link } from "react-router-dom";
-// import logo from "../assets/images/tbr-logo.png";
+import Input from "../ui/Input";
 
 function Login() {
   return (
@@ -16,30 +14,16 @@ function Login() {
       </Link>
       <div className="py-16 flex items-center justify-center">
         <div className="max-xl:px-6 max-w-xl w-full flex flex-col justify-center items-center h-full ">
-          {/* <img src={logo} className="w-40" /> */}
-
           <h1 className="text-3xl font-bold text-center">Welcome back </h1>
           <p className="mt-1 text-gray-500  text-center">
             Enter your email and password to continue your journey with us.
           </p>
           <form className="mt-8 space-y-4 w-full">
-            <label className="flex flex-col">
-              <span className="">Email:</span>
-              <input
-                type="email"
-                placeholder="Enter your email address"
-                className="py-3 px-3 placeholder:text-sm rounded-lg bg-mainWhite shadow-sm outline-none border border-transparent duration-150 focus:border-mainLight"
-              />
-            </label>
+            <Input type="email" label="Email address" />
+
             <div className="flex flex-col  space-y-2">
-              <label className="flex flex-col">
-                <span className="">Password:</span>
-                <input
-                  type="password"
-                  placeholder="Enter your password"
-                  className="py-3 px-3 placeholder:text-sm rounded-lg bg-mainWhite shadow-sm outline-none border border-transparent duration-150 focus:border-mainLight"
-                />
-              </label>
+              <Input type="password" label="Password" />
+
               <p className="ml-auto text-gray-500 text-sm">
                 Forgot password?{" "}
                 <a href="#" className="text-mainBlack font-semibold">
---
+++ src/ui/Input.jsx
@@ -0,0 +1,25 @@
+import { twMerge } from "tailwind-merge";
+import PropTypes from "prop-types";
+
+function Input({ className, label, type }) {
+  return (
+    <label className={twMerge("flex flex-col", className)}>
+      <span>{label}:</span>
+      <input
+        type={type}
+        placeholder={`Enter your ${label.toLowerCase()} ${
+          label.toLowerCase() === "confirm password" ? "again" : ""
+        }`}
+        className="py-3 px-3 placeholder:text-sm rounded-lg bg-mainWhite shadow-sm outline-none border border-transparent duration-150 focus:border-mainLight"
+      />
+    </label>
+  );
+}
+
+Input.propTypes = {
+  className: { type: PropTypes.string },
+  label: { type: PropTypes.string, require: true },
+  type: { type: PropTypes.string, require: true },
+};
+
+export default Input;
